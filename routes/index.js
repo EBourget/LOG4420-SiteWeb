a01f24f5595c08145adc8899406a2d148ee9703f
Edwin Bourget <edwin.bourget@live.fr>
2014-11-06 17:17:09 -0500

    Mise en place des vues, des feuilles de style et des script
    Signed-off-by: Edwin Bourget <edwin.bourget@live.fr>
    
    Signed-off-by: Edwin Bourget <edwin.bourget@live.fr>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/index', function(req, res) {
+  res.redirect('/');
+});
+
+router.get('/dashboard', function(req, res) {
+  res.render('dashboard', { titre: 'Express' });
+});
+
+router.get('/instructions', function(req, res) {
+  res.render('instructions', { titre: 'Express' });
+});
+
+router.get('/examen', function(req, res) {
+  res.render('examen', { titre: 'Express' });
+});
+
+router.get('/quicktest', function(req, res) {
+  res.render('quicktest', { titre: 'Express' });
+});
+
+router.get('/results', function(req, res) {
+  res.render('results', { titre: 'Express' });
+});
+
 
 module.exports = router;
