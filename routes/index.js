8a1cf0a92b486be695cb94b0e9f8ed4cbd8dbc36
Edwin Bourget <edwin.bourget@live.fr>
2014-11-06 23:37:50 -0500

    Préparation de la gestion des sessions (questions 6 et 7)
    Signed-off-by: Edwin Bourget <edwin.bourget@live.fr>

--- routes/index.js
+++ routes/index.js
@@ -3,39 +3,49 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { titre: 'Accueil', index: true });
+	res.render('index', { titre: 'Accueil', index: true });
 });
 
 router.get('/index', function(req, res) {
-  res.redirect('/');
+	res.redirect('/');
 });
 
 router.get('/dashboard', function(req, res) {
-  res.render('dashboard', { titre: 'Tableau de bord', dashboard: true });
+	res.render('dashboard', { titre: 'Tableau de bord', dashboard: true });
 });
 
 router.get('/instructions', function(req, res) {
-  res.render('instructions', { titre: 'Instructions', instructions: true });
+	res.render('instructions', { titre: 'Instructions', instructions: true });
 });
 
-router.get('/examen', function(req, res) {
-  res.render('examen', { titre: 'Examen' });
+router.get('/examen', function(req, res, next) {
+	next();
 });
 
-router.post('/examen', function(req, res) {
-  res.render('examen', { titre: 'Examen' });
+router.post('/examen', function(req, res, next) {
+	req.session.nombreQuestions = req.body.nombre_questions;
+	var sujets = new Array();
+	next();
 });
 
-router.get('/quicktest', function(req, res) {
-  res.render('quicktest', { titre: 'Test rapide' });
+router.all('/examen', function(req,res) {
+	res.render('examen', { titre: 'Examen' });
 });
 
-router.post('/quicktest', function(req, res) {
-  res.render('quicktest', { titre: 'Test rapide' });
+router.get('/quicktest', function(req, res, next) {
+	next();
+});
+
+router.post('/quicktest', function(req, res, next) {
+	next();
+});
+
+router.all('/quicktest', function(req,res) {
+	res.render('quicktest', { titre: 'Test rapide', nomDomaine: 'HTML, CSS ou JS', question: "Question ?!", reponses: ['Réponse 1', 'Réponse 2', 'Réponse 3'], idReponse: 2 });
 });
 
 router.get('/results', function(req, res) {
-  res.render('results', { titre: 'R&eacute;sultats' });
+	res.render('results', { titre: 'R&eacute;sultats' });
 });
 
 
