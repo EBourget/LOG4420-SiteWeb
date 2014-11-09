8b674f8cd4a5a4fa58db699c9f24844b519a027f
Edwin Bourget <edwin.bourget@live.fr>
2014-11-09 17:32:24 -0500

    Renomage du fichier de gestion des routes
    Signed-off-by: Edwin Bourget <edwin.bourget@live.fr>

--- routes/routes.js
+++ routes/routes.js
@@ -12,11 +12,15 @@ router.get('/index', function(req, res) {
 });
 
 router.get('/dashboard', function(req, res) {
-	/*affichage des domaines*/
+	/*affichage des domaines et recherche du nombre max de question par domaine*/
 	var domaines = new Array();
+	var maxQuestion = new Array();
 	for(i=0; i<db.getNombreDomaines(); i++)
-		domaines.push(db.getDomaineById(i))
-	res.render('dashboard', { titre: 'Tableau de bord', dashboard: true, domaines: domaines });
+	{
+		domaines.push(db.getDomaineById(i));
+		maxQuestion.push(db.getMaxQuestion(i));
+	}
+	res.render('dashboard', { titre: 'Tableau de bord', dashboard: true, domaines: domaines,maxQuestion: maxQuestion});
 });
 
 router.get('/instructions', function(req, res) {
@@ -24,14 +28,11 @@ router.get('/instructions', function(req, res) {
 });
 
 router.post('/examen', function(req, res, next) {
+	// on récupère les informations du formulaire
 	req.session.nombreQuestions = req.body.nombre_questions;
-	var domaines = ['HTML', 'CSS'];
-	/*for (i=0; i<db.getNombreDomaines(); i++)
-	{
-		monDomaine = db.getDomaineById(i);
-		if(req.body.monDomaine!= undefined)
-			domaines.push(req.body.monDomaine);
-	}*/
+	var domaines = req.body.domaines;
+	if(typeof domaines == "string") // s'il n'y a qu'un domaine on le mets dans un tableau
+	domaines =[domaines];
 	// creer un tableau avec les id des questions correspondants à sujet
 	var tabQuestions = db.getQuestionsExamen(domaines);
 	// sauvegarder ce tableau dans la session
@@ -40,24 +41,22 @@ router.post('/examen', function(req, res, next) {
 });
 
 router.get('/examen', function(req, res, next) {
-
 	next();
 });
 
 router.all('/examen', function(req,res) {
 	var tabQuestions = JSON.parse(req.session.tabQuestions);
 	// choisir une question au hasard et on la supprime du tableau
-	idQuestion = db.getQuestionAleatoireExamen(tabQuestions);
+	maQuestion = db.getQuestionAleatoireExamen(tabQuestions);
 	// sauvegarder le nouveau tableau
 	req.session.tabQuestions = JSON.stringify(tabQuestions);
-	// récupérer la question
-	var maQuestion = db.getQuestionById(idQuestion);
-	var nomDomaine = db.getDomaineById(maQuestion.domaine);
-	var enonce = maQuestion.question;
-	var choix = maQuestion.choix;
-	var solution = maQuestion.reponse;
+	// récupérer les attributs de la question
+	var nomDomaine = db.getDomaine(maQuestion);
+	var enonce = db.getQuestion(maQuestion);
+	var choix = db.getChoix(maQuestion);
+	var solution = db.getReponse(maQuestion);
 	
-	res.render('examen', { titre: 'Examen', nomDomaine: nomDomaine, question: enonce, reponses: choix, idReponse: solution});
+	res.render('questions', { examen: true, titre: 'Examen', nomDomaine: nomDomaine, question: enonce, reponses: choix, idReponse: solution});
 });
 
 router.post('/quicktest', function(req, res, next) {
@@ -70,11 +69,11 @@ router.get('/quicktest', function(req, res, next) {
 
 router.all('/quicktest', function(req,res) {
 	var maQuestion = db.getQuestionAleatoireTest();
-	var nomDomaine = db.getDomaineById(maQuestion.domaine);
-	var enonce = maQuestion.question;
-	var choix = maQuestion.choix;
-	var solution = maQuestion.reponse;
-	res.render('quicktest', { titre: 'Test rapide', nomDomaine: nomDomaine, question: enonce, reponses: choix, idReponse: solution});
+	var nomDomaine = db.getDomaine(maQuestion);
+	var enonce = db.getQuestion(maQuestion);
+	var choix = db.getChoix(maQuestion);
+	var solution = db.getReponse(maQuestion);
+	res.render('questions', { examen: false, titre: 'Test rapide', nomDomaine: nomDomaine, question: enonce, reponses: choix, idReponse: solution});
 });
 
 router.get('/results', function(req, res) {
